7a85ed07d7ce546a5f2d413a6c342bb6cd97c024
Sherman <shc410@gmail.com>
2017-03-16 23:42:21 -0700

    map ids in state to props

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 
 export default class Movies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imdbIDs: null
+    };
+    this.handleDetails = this.handleDetails.bind(this);
+  }
+
+  handleDetails() {
+    console.log('here');
+  }
+
+  componentWillMount() {
+    let { movies } = this.props;
+
+    let mappedImdbIds = movies.map((movie) => {
+      return movie.imdbID;
+    });
+
+    this.setState({
+      imdbIDs: mappedImdbIds
+    });
+  }
+
   render() {
     /*
     Title,
-    Rated,
-    Released,
-    Director,
-    Plot,
-    Poster
+    imdbID,
+    Poster,
+    Year,
+    Type
     */
 
 
     let { movies } = this.props;
 
-    let mappedMovies = movies.map((movie) => {
+    let mappedMovies = movies.map((movie, index) => {
+      console.log(`${movie.imdbID}` === `${this.state.imdbIDs[index]}`);
       return (
         <section key={movie.imdbID}>
-          <h3>{movie.Title}</h3>
-          <img src={`${movie.Poster}`}></img>
+          <h3>{movie.Title} - {movie.Year}</h3>
+          <img src={`${movie.Poster}`} alt={`Movie Poster for ${movie.Title}`}></img>
+          <button onClick={this.handleDetails}>Details</button>
         </section>
       )
     });
