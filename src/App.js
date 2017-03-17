995d7fb1f330f2cb100dd308be91d95f9bc36cac
Sherman <shc410@gmail.com>
2017-03-16 22:20:12 -0700

    set up file structure and home component

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-const App = () => <h1>TACOCAT</h1>
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './Home';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path="/" component={Home} />
+      
+    </div>
+  </Router>
+);
+
+export default App;
